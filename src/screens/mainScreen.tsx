61c670885e68caec9110b5fc889a64928997fdf8
Mimenza <mimenzae@gmail.com>
2025-04-24 19:48:44 +0200

    lateral menu improved

--- src/screens/mainScreen.tsx
+++ src/screens/mainScreen.tsx
@@ -21,11 +21,11 @@ const MainScreen = () => {
   const navigate = useNavigate();
   const { logedUser, setLogedUser } = useLogedUser();
 
-   useEffect(() => {
-      if (!logedUser) {
-        navigate("/login");
-      }
-    }, []);
+  useEffect(() => {
+    if (!logedUser) {
+      navigate("/login");
+    }
+  }, []);
 
   const sections = useMemo(
     () => [aboutMeRef, projectsRef, technologiesRef, contactRef],
@@ -100,28 +100,56 @@ const MainScreen = () => {
   return (
     <div className="flex flex-col min-h-screen w-screen bg-black overflow-hidden relative">
       {/* Section Indicator */}
-      <div className="fixed top-1/2 right-[-120px] transform -translate-y-1/2 flex flex-col gap-2 z-20 group transition-all duration-300 transform hover:translate-x-[-130px]">
+      <div className="fixed top-1/2 right-[-142px] transform -translate-y-1/2 z-20 group transition-all duration-300 hover:translate-x-[-130px]">
         {sections.map((_, index) => (
-          <div key={index} className="grid grid-cols-2 items-center">
-            <div
-              className={`w-3 h-3 rounded-full cursor-pointer transition-transform duration-300 ${
-                currentSectionIndex === index
-                  ? "bg-white scale-125"
-                  : "bg-gray-500"
-              } hover:bg-gray-200 active:scale-75`}
-              onClickCapture={() => handleIndicatorClick(index)}
-              style={{ justifySelf: "center" }}
-            />
-            <h1
-              className={`cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${
-                currentSectionIndex === index ? "text-white" : "text-gray-500"
-              } hover:text-gray-200`}
-              onClick={() => handleIndicatorClick(index)}
-            >
-              {sectionTitles[index]}
-            </h1>
+          <div
+            key={index}
+            className={`flex items-center bg-gray-600 bg-opacity-25 ${
+              index === 0 ? "rounded-tl-xl" : ""
+            } ${index === sections.length - 1 ? "rounded-bl-xl" : ""}`}
+          >
+            {/* Columna de los círculos */}
+            <div className="flex items-center justify-center w-10 h-10 mr-4">
+              <div
+                className={`w-3 h-3 rounded-full cursor-pointer transition-transform duration-300 ${
+                  currentSectionIndex === index
+                    ? "bg-white scale-[155%]"
+                    : "bg-gray-500"
+                } hover:bg-gray-200 active:scale-75`}
+                onClickCapture={() => handleIndicatorClick(index)}
+              />
+            </div>
+
+            {/* Columna de los nombres */}
+            <div className="flex items-center justify-start mr-5">
+              <h1
+                className={`cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${
+                  currentSectionIndex === index ? "text-white" : "text-gray-500"
+                } hover:text-gray-200`}
+                onClick={() => handleIndicatorClick(index)}
+              >
+                {sectionTitles[index]}
+              </h1>
+            </div>
           </div>
         ))}
+
+        {/* Nuevas dos columnas estáticas debajo */}
+        <div className="flex">
+          {/* <div className="w-10 h-15 bg-gray-600 bg-opacity-25">
+            <div className="w-full h-full bg-black rounded-tr-xl"></div>
+          </div> */}
+          <div className="w-10 h-15" />
+          <div className="flex flex-col justify-center pl-4 rounded-bl-xl bg-gray-600 bg-opacity-25 w-[calc(100%-2.5rem)]">
+            <div className="border-t border-gray-600 h-1 w-[100%] mx-auto self-center" />
+            <h1 className="cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:text-white text-gray-500 my-1">
+              Projects
+            </h1>
+            <h1 className="cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:text-white text-gray-500 my-1">
+              CV here
+            </h1>
+          </div>
+        </div>
       </div>
 
       {/* Sections */}
