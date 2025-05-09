ce654a3629e8013061a767f6393a6d31e879cda1
Mimenza <mimenzae@gmail.com>
2025-05-09 13:26:21 +0200

    Missing test Screen

---
+++ src/screens/test.tsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Test = () => {
+ 
+
+  return (
+    <div
+        className="w-full h-screen flex items-center justify-center"
+    >
+        <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">Test</h1>
+        </div>
+    </div>
+  );
+};
+
+
+export default Test;
